5d0ffac462a6d6240fb14f0c3851b81e319f2ff8
Mukesh Prajapat <darwin.mukesh2022@gmail.com>
2023-06-15 12:27:14 +0530

    random checks

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,36 @@
-import { useState } from "react"
-import Gallery from './imageGallery'
-
-// import './App.css'
-// import Form from "./form"
-
-// const blogsubmit = ()=>{
-//   return alert("formsubmitted");
-// }
-{/* <Form onformSubmit={blogsubmit}/> */}
+import { useState } from "react";
+import Gallery from "./imageGallery";
 
 export default function App() {
-  const [count,setCount] = useState(0);
-  const [index,setindex] = useState(0);
+  const [count, setCount] = useState(0);
+  const [index, setindex] = useState(0);
 
-  const brry = ["mukesh","suresh","arjun"];
-  // const {name,age} = props
-  
-  const handleClick = ()=>{
-    if(count < 10){
-      setCount(count+1)
+  const brry = ["mukesh", "suresh", "arjun"];
+
+  const handleClick = () => {
+    if (count < 10) {
+      setCount(count + 1);
     }
-  //   console.log(count);
-  // setCount(count+1)
-  // console.log(count)
-    
-  }
+  };
 
-  const MinusHandle = () =>{
-    if(count > 0){
-      setCount(count-1);
+  const MinusHandle = () => {
+    if (count > 0) {
+      setCount(count - 1);
     }
-    
-  }
+  };
 
-  const newName = ()=>{
-    if(index<brry.length-1){
-      setindex(index+1)
+  const newName = () => {
+    if (index < brry.length - 1) {
+      setindex(index + 1);
     }
 
-    if(index==2){
+    if (index == 2) {
       setindex(0);
     }
-  }
+  };
   // <p>{brry[index]}</p>
   //    <button onClick={newName}>next</button>
   //    <button onClick={handleClick}>+</button>
   //    <button onClick={MinusHandle}>-</button>
-  return (
-     <Gallery />
-
-  )
+  return <Gallery />;
 }
-
-
-
